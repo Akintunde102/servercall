ef91cf9429aead079cd6131ec26773fd98a23395
Akintunde Jegede <akintunde.jegede@primarybid.com>
2022-03-14 04:55:19 +0100

    1.0.21

--- src/tests/resources/fake-server-call-store.ts
+++ src/tests/resources/fake-server-call-store.ts
@@ -2,7 +2,7 @@ import { ServerCallVerbs, ServerCallsType } from '../../types';
 
 export type ServerCallsKeyType = 'userExists' | 'sendShortCode' | 'getUser';
 
-export const serverCalls: ServerCallsType<ServerCallsKeyType> ={
+export const serverCalls: ServerCallsType<ServerCallsKeyType> = {
   userExists: { path: `/users/exists`, verb: ServerCallVerbs.Post, name: 'UserExists' },
   sendShortCode: { path: `/users/send/short-code`, verb: ServerCallVerbs.Post, name: 'SendShortCode' },
   getUser: { path: (args: { id: string }) => `/users/id/${args.id}`, verb: ServerCallVerbs.Get, name: 'GetUser' },
