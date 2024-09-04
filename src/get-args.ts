1058b0aa06b7ffc664dbe2823b547cdb112c0ca7
Akintunde Jegede <akintunde.jegede@primarybid.com>
2024-09-05 00:38:59 +0100

    change forloop to forOf

--- src/get-args.ts
+++ src/get-args.ts
@@ -1,9 +1,9 @@
 /** Get Arguments from Process */
 export const getArgs = <T>(process: any): T => {
     const cmdArgs = process.argv;
-    const cmdArgsObject  = {} as T | any;
-    for (let index = 0; index < cmdArgs.length; index++) {
-        const [arg, value] = cmdArgs[index].split('=');
+    const cmdArgsObject = {} as T | any;
+    for (const cmdArg of cmdArgs) {
+        const [arg, value] = cmdArg.split('=');
         if (value) {
             cmdArgsObject[arg.replace(/-/g, '')] = value;
         }
